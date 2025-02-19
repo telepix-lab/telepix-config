c56f31bdfbe96f55c19f21c5f0566e6723c01d0b
김지동 <apitots@kakao.com>
2025-02-19 15:23:29 +0900

    ✅ Refactor(ESLint): 출력 결과 ESM, CJS 모두 지원

--- packages/eslint-config/rollup.config.js
+++ packages/eslint-config/rollup.config.js
@@ -3,15 +3,23 @@ import json from "@rollup/plugin-json";
 
 export default {
   input: "index.mjs",
-  output: {
-    dir: "dist",
-    format: "cjs",
-    sourcemap: true,
-    preserveModules: true,
-    preserveModulesRoot: ".",
-    exports: "auto",
-    interop: "auto",
-  },
+  output: [
+    {
+      dir: "dist/esm",
+      format: "esm",
+      sourcemap: true,
+      preserveModules: true,
+      preserveModulesRoot: ".",
+    },
+    {
+      dir: "dist/cjs",
+      format: "cjs",
+      sourcemap: true,
+      preserveModules: true,
+      preserveModulesRoot: ".",
+      exports: "auto",
+    },
+  ],
   plugins: [
     commonjs({
       requireReturnsDefault: "auto",
